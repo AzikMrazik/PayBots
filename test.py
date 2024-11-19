e999ad782800086d0889b87646758ea87608d80e
AzikMrazik <82385429+AzikMrazik@users.noreply.github.com>
2024-11-19 14:00:08 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import logging
 from pyrogram import Client, filters
 
 logging.basicConfig(
-    level=logging.DEBUG,  # Уровень логирования (DEBUG для подробных логов)
+    level=logging.INFO,  # Уровень логирования (DEBUG для подробных логов)
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
@@ -20,28 +20,30 @@ target_channel_id = -1002415709971  # ID канала (назначение)
 # Инициализация клиента
 app = Client(session_name, api_id=api_id, api_hash=api_hash)
 
+@app.on_message()
 async def forward_message(client, message):
     try:
         # Проверяем, что сообщение пришло из нужной группы
         if message.chat.id == source_chat_id:
-            # Пересылаем сообщение
             await client.forward_messages(chat_id=target_channel_id, from_chat_id=message.chat.id, message_ids=message.id)
-            logging.info(f"Сообщение переслано: {message.text}")
+            logger.info(f"Сообщение переслано: {message.text}")
         else:
-            logging.debug(f"Сообщение из другого чата: {message.chat.id}")
+            logger.debug(f"Сообщение из другого чата: {message.chat.id}")
     except Exception as e:
-        logging.error(f"Ошибка при пересылке сообщения: {e}")
+        logger.error(f"Ошибка при пересылке сообщения: {e}")
 
 async def main():
     try:
-        await app.start()
-        logging.info("Клиент запущен и готов к работе.")
-        await asyncio.Event().wait()  # Бесконечное ожидание
+        if not app.is_connected:
+            await app.start()
+        logger.info("Клиент запущен и готов к работе.")
+        await asyncio.Event().wait()  # Ожидание
     except Exception as e:
-        logging.critical(f"Критическая ошибка: {e}")
+        logger.critical(f"Критическая ошибка: {e}")
     finally:
-        await app.stop()
-        logging.info("Клиент остановлен.")
+        if app.is_connected:
+            await app.stop()
+            logger.info("Клиент остановлен корректно.")
 
 if __name__ == "__main__":
     asyncio.run(main())
