b17ad1023ccf6bb585d26df6d3d1b321675a173b
AzikMrazik <82385429+AzikMrazik@users.noreply.github.com>
2024-11-21 04:05:44 +0500

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -14,17 +14,20 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# Ваши данные
+# Загрузка данных
 api_id = int(os.getenv('API_ID'))  # Замените на ваш API ID
-api_hash = os.getenv('API_HASH') # Замените на ваш API Hash
+api_hash = os.getenv('API_HASH')  # Замените на ваш API Hash
 session_name = "boter"  # Имя сессии
 
 # ID чатов
-source_chat_id = int(os.getenv('SOURCE_CHAT_ID')) # ID группы (источник)
+source_chat_id = int(os.getenv('SOURCE_CHAT_ID'))  # ID группы (источник)
 target_channel_id = int(os.getenv('TARGET_CHANNEL_ID'))  # ID канала (назначение)
 
-print({api_id},{api_hash},{type(source_chat_id)},{type(target_channel_id)})
+# ID пользователя для фильтрации
+user_id_filter = 6987651297  # ID пользователя, чьи сообщения нужно пересылать
 
+# Проверка данных
+logger.info(f"API_ID: {api_id}, API_HASH: {api_hash}, SOURCE_CHAT_ID: {source_chat_id}, TARGET_CHANNEL_ID: {target_channel_id}")
 
 # Инициализация клиента
 app = Client(session_name, api_id=api_id, api_hash=api_hash)
@@ -34,9 +37,13 @@ async def forward_message(client, message):
     try:
         # Проверяем, что сообщение пришло из нужной группы
         if message.chat.id == source_chat_id:
-            logger.info(f"Пересылка сообщения из {message.chat.id} в {target_channel_id}")
-            await client.forward_messages(chat_id=target_channel_id, from_chat_id=message.chat.id, message_ids=message.id)
-            logger.info(f"Сообщение переслано: {message.text}")
+            # Проверяем, что сообщение отправлено указанным пользователем
+            if message.from_user and message.from_user.id == user_id_filter:
+                logger.info(f"Пересылка сообщения от {message.from_user.id} из {message.chat.id} в {target_channel_id}")
+                await client.forward_messages(chat_id=target_channel_id, from_chat_id=message.chat.id, message_ids=message.id)
+                logger.info(f"Сообщение переслано: {message.text}")
+            else:
+                logger.debug(f"Сообщение от другого пользователя: {message.from_user.id if message.from_user else 'Неизвестно'}")
         else:
             logger.debug(f"Сообщение из другого чата: {message.chat.id}")
     except Exception as e:
