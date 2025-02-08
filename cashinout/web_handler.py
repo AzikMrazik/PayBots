129929316d0cacbedcb20af5d1fb4c57241d7563
AzikMrazik <AzikMrazik@yandex.ru>
2025-02-09 01:20:14 +0500

    Update web_handler.py

--- cashinout/web_handler.py
+++ cashinout/web_handler.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 async def handle_post(request: web.Request):
     bot: Bot = request.app['bot']
-    logger = logging.getLogger(__name__)
     try:
         logger.info("Handling POST request to /custom_webhook")
         data = await request.json()
@@ -24,6 +23,8 @@ async def start_web_app(dispatcher: Dispatcher, bot: Bot):
     app = web.Application()
     app['bot'] = bot  # Сохраняем бот в приложении
     
+    app.router.add_get('/test', handle_test)
+
     SimpleRequestHandler(
         dispatcher=dispatcher, 
         bot=bot,
@@ -32,3 +33,6 @@ async def start_web_app(dispatcher: Dispatcher, bot: Bot):
     
     app.router.add_post('/custom_webhook', handle_post)
     return app
+
+async def handle_test(request):
+    return web.Response(text="OK")
