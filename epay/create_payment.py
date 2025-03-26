35c611afd3f27340b8bfeba8a7ec710de917914e
AzikMrazik <AzikMrazik@yandex.ru>
2025-03-27 02:00:16 +0500

    QoL upd

--- epay/create_payment.py
+++ epay/create_payment.py
@@ -42,26 +42,13 @@ async def create_payment(message: Message,  state: FSMContext):
         await message.answer("Отправьте новое значение:", reply_markup=back_kb())
         return
     else:
-        bot_msg = await message.reply("⌛️Ожидаем реквизиты...")
-        checkout = await sendpost(amount, message.from_user.id, 1)
-        await bot_msg.delete()
-        if checkout == True:
-            await message.reply("⛔Нет реквизитов!")
-        else:
-            await message.reply(checkout[0])
-            await message.answer(checkout[1])
+        msg = await message.reply("⌛️Ожидаем реквизиты...")
+        order = await sendpost(amount, message.from_user.id, msg, 1)
+        await msg.delete()
+        for i in order:
+            await message.answer(i)
         await message.answer("Введите сумму для следующего платежа:", reply_markup=back_kb())
         await state.set_state(PaymentStates.WAITING_AMOUNT)
-
-async def bank_check(bin):
-    async with connect("bins.db") as db:
-        cursor = await db.execute(
-            "SELECT note FROM bins WHERE bin = ?", 
-            (bin,)
-        )
-        result = await cursor.fetchone()
-        resultend = result[0]
-        return resultend
     
 async def check_name(bin):
     async with connect("bins.db") as db:
@@ -73,7 +60,7 @@ async def check_name(bin):
         resultend = result[0]
         return resultend
 
-async def sendpost(amount, chat_id, counter):
+async def sendpost(amount, chat_id, msg, counter):
     merchant_order_id = datetime.now().strftime("%d%m%H%M")
     async with ClientSession() as session:
         async with session.post(
@@ -88,7 +75,8 @@ async def sendpost(amount, chat_id, counter):
             try:
                 data = await response.json()
             except:
-                return (f"⚰️", f"E-Pay отправил труп!")
+                data = await response.text()
+                return (f"⚰️", f"E-Pay отправил труп!", {data}, "Отправьте сообщение выше кодеру!")
             else:
                 order_status = data['status']
                 print(data, flush=True)
@@ -104,39 +92,26 @@ async def sendpost(amount, chat_id, counter):
                         sbp = False
                     if not sbp:
                         bin = card[:6]
+                        bank_name = await check_name(bin)
                         bank_type = "карты"
                         if bin[:3] != "220":
-                            print("again non-ru")
                             await asyncio.sleep(3)
-                            if counter < 5:
+                            if counter < 6:
                                 counter += 1
-                                await asyncio.sleep(3)
-                                return await sendpost(amount, chat_id, counter)
-                        bank_status = await bank_check(bin)
+                                await msg.edit_text(f"⌛️Ожидаем реквизиты...({counter}/5)")
+                                return await sendpost(amount, chat_id, msg, counter)
+                            else:
+                                return ("⛔Нет реквизитов",)
                     else:
-                        bank_status = "Good"
                         bank_type = "телефона"
-                    bank_name = await check_name(bin)
-                    if bank_status != "RIP":
-                        await addorder(order_id, chat_id, precise_amount)
-                        return (f"📄 Создан заказ: №<code>{order_id}</code>\n\n💳 Номер {bank_type} для оплаты: <code>{card}</code>\n💰Сумма платежа: <code>{precise_amount}</code> рублей\n\n🕑 Время на оплату: 30 мин.", F"🏦Банк: {bank_name}")
-                    else:
-                        print("again RIP")
-                        await asyncio.sleep(3)
-                        if counter < 5:
-                            counter += 1
-                            await asyncio.sleep(3)
-                            return await sendpost(amount, chat_id, counter)
-                        else:
-                            return True
+                    await addorder(order_id, chat_id, precise_amount)
+                    return (f"📄 Создан заказ: №<code>{order_id}</code>\n\n💳 Номер {bank_type} для оплаты: <code>{card}</code>\n💰Сумма платежа: <code>{precise_amount}</code> рублей\n\n🕑 Время на оплату: 30 мин.", F"🏦Банк: {bank_name}")
                 else:
-                        print("again no")
-                        print(counter)
-                        if counter < 5:
-                            counter += 1
-                            await asyncio.sleep(3)
-                            return await sendpost(amount, chat_id, counter)
+                        if counter < 6:
+                                counter += 1
+                                await msg.edit_text(f"⌛️Ожидаем реквизиты...({counter}/5)")
+                                return await sendpost(amount, chat_id, msg, counter)
                         else:
-                            return True                   
+                            return ("⛔Нет реквизитов",)                
 
 
--- epay/epay.py
+++ epay/epay.py
@@ -1,6 +1,5 @@
 import logging
 import asyncio
-from aiosqlite import connect
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import Command
@@ -9,7 +8,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 import create_payment, group_payment, checker, balance
-from config import BOT_TOKEN, ALLOWED_GROUPS
+from config import BOT_TOKEN
 from checker import checklist
 
 logging.basicConfig(level=logging.INFO)
@@ -37,49 +36,6 @@ async def start_command(message: Message, state: FSMContext):
     await state.clear()
     await message.answer("Добро пожаловать!")
     await message.answer(text="Вы в главном меню, выберите действие:", reply_markup=main_kb())
-
-@dp.message(F.chat.type.in_({"group", "supergroup"}), F.text.startswith("/ban_"))
-async def cash_command(message: Message):
-    if message.chat.id not in ALLOWED_GROUPS:
-        await message.answer("Бот не активирован в этой группе!")
-        return
-    try:
-        bin = message.text.split("_")[1]
-        async with connect("bins.db") as db:
-            await db.execute(
-            "UPDATE bins SET note = 'RIP' WHERE bin = ?",
-            (bin,)
-            )
-            await db.commit()
-    except:
-        await message.answer("Неверный формат команды. Используйте: /ban_220501")
-        return
-    else:
-        bot_msg = await message.answer(f"⛔BIN {bin} успешно забанен!")
-        await asyncio.sleep(10)
-        await bot_msg.delete()
-
-@dp.message(F.chat.type.in_({"group", "supergroup"}), F.text.startswith("/unban_"))
-async def cash_command(message: Message):
-    if message.chat.id not in ALLOWED_GROUPS:
-        await message.answer("Бот не активирован в этой группе!")
-        return
-    try:
-        bin = message.text.split("_")[1]
-        print(bin)
-        async with connect("bins.db") as db:
-            await db.execute(
-                "UPDATE bins SET note = '' WHERE bin = ?",
-                (bin,)
-                )             
-            await db.commit()
-    except Exception as e:
-        await message.answer(f"{e}Неверный формат команды. Используйте: /unban_220501")
-        return
-    else:
-        bot_msg = await message.answer(f"✅BIN {bin} успешно разбанен!")
-        await asyncio.sleep(10)
-        await bot_msg.delete()
 
 @dp.message(Command("ping"))
 async def start_command(message: Message):
