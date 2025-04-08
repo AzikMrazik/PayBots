a9cbb02f9c2744f92abac8d2432f78160bd31f35
AzikMrazik <AzikMrazik@yandex.ru>
2025-04-08 15:26:02 +0500

    Update notificator.py

--- notificator/notificator.py
+++ notificator/notificator.py
@@ -4,16 +4,10 @@ from aiohttp import web
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
-import logging
-from aiosqlite import connect
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler
-from aiogram import Bot, Dispatcher
-from aiogram.utils.formatting import *
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN, DOMAIN, REPORT_CHAT_ID, ADMINS
-from aiohttp import web 
-from aiogram.webhook.aiohttp_server import setup_application
 from urllib.parse import parse_qs
 from datetime import datetime, timedelta
 import pandas as pd
@@ -25,7 +19,7 @@ from datetime import time as dt_time
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()]  # Явное указание вывода в консоль
+    handlers=[logging.StreamHandler()]
 )
 logger = logging.getLogger(__name__)
 
@@ -34,12 +28,16 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 COMMISSION_RATES = {
-    "epay": lambda x: 0.32 if x < 10000 else 0.25,
+    "epay": 0.35,
     "corkpay": 0.30,
     "apay": 0.10,
     "crocopay": 0.15,
-    "cashinout": 0.10,
-    "p2p": 0.26
+}
+
+DB_PATHS = {
+    "corkpay": "/root/paybots/corkpay/orders_corkpay.db",
+    "epay": "/root/paybots/epay/orders_epay.db",
+    "crocopay": "/root/paybots/crocopay/orders_crocopay.db"
 }
 
 def calculate_net_amount(system: str, amount: float) -> float:
@@ -47,6 +45,44 @@ def calculate_net_amount(system: str, amount: float) -> float:
     if callable(rate):
         return amount * (1 - rate(amount))
     return amount * (1 - rate)
+
+async def process_webhook(request: web.Request, system: str, message_template: str):
+    bot: Bot = request.app['bot']
+    try:
+        data = await request.json() if system != "corkpay" else await request.text()
+        logger.info(f"Получен вебхук для {system}: {data}")
+
+        if system == "corkpay":
+            cleaned_text = data.lstrip('✅ \n\t')
+            parsed_data = parse_qs(cleaned_text)
+            data = {k: v[0] for k, v in parsed_data.items()}
+
+        order_id = data.get('transaction_id') or data.get('merchant_order') or request.match_info.get('order_id')
+        amount = data.get('total') or data.get('amount')
+
+        chat_id, amount = await get_chat_id(order_id, system)
+        if chat_id is None:
+            return web.Response(text="OK")
+
+        await bot.send_message(
+            chat_id=int(chat_id),
+            text=message_template.format(order_id=order_id, amount=amount)
+        )
+        await add_paid_order(float(amount), int(chat_id), system)
+
+        return web.Response(text="OK", status=200)
+    except Exception as e:
+        logger.error(f"Ошибка обработки вебхука для {system}: {e}")
+        return web.Response(text="OK", status=200)
+
+async def handle_corkpay(request: web.Request):
+    return await process_webhook(request, "corkpay", "🟣CORKPAY:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!")
+
+async def handle_epay(request: web.Request):
+    return await process_webhook(request, "epay", "🟡E-PAY:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!")
+
+async def handle_crocopay(request: web.Request):
+    return await process_webhook(request, "crocopay", "🟢CrocoPay:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!")
 
 async def start_web_app(dispatcher: Dispatcher, bot: Bot):
     app = web.Application()
@@ -65,148 +101,35 @@ async def start_web_app(dispatcher: Dispatcher, bot: Bot):
 async def handle_root(request):
     return web.Response(text="Forbidden", status=403)
 
-async def handle_corkpay(request: web.Request):
-    bot: Bot = request.app['bot']
-    system = "corkpay"
-    try:
-        data = await request.text()
-        logger.info(f"Получен вебхук: {data}")
-        cleaned_text = data.lstrip('✅ \n\t')
-        parsed_data = parse_qs(cleaned_text)
-        data = {k: v[0] for k, v in parsed_data.items()}
-        try:
-            order_id = data['merchant_order']
-            chat_id, amount = await get_chat_id(order_id, system)
-            if chat_id != None:
-                chat_id = int(chat_id)
-            else:
-                return web.Response(text="OK")
-            try:
-                await bot.send_message(
-                    chat_id=chat_id,
-                    text=f"🟣CORKPAY:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!"
-                )
-                await add_paid_order(float(amount), chat_id, "corkpay")
-            except Exception as e:
-                logger.info(f"Ошибка: {e}")
-        except Exception as e:   
-                logger.info(f"Ошибка: {e}")
-        return web.Response(text="OK", status=200)
-    except Exception as e:
-        logger.error(f"Ошибка: {str(e)}")
-        return web.Response(text="OK", status=200)
-
-async def handle_epay(request: web.Request):
-    bot: Bot = request.app['bot']
-    system = "epay"
-    try:
-        data = await request.json()
-        logger.info(f"Получен вебхук: {data}")
-        order_id = data['transaction_id']
-        chat_id, amount = await get_chat_id(order_id, system)
-        if chat_id != None:
-                chat_id = int(chat_id)
-        else:
-                return web.Response(text="OK")
-        try:
-            try:
-                await bot.send_message(
-                    chat_id=chat_id,
-                    text=f"🟡E-PAY:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!"
-                )
-                await add_paid_order(float(amount), chat_id, "epay")
-            except Exception as e:
-                logger.info(f"Ошибка №1: {e}")
-        except Exception as e:   
-                logger.info(f"Ошибка №2: {e}")
-
-        return web.Response(text="OK", status=200)
-    
-    except Exception as e:
-        logger.error(f"Ошибка: {str(e)}")
-        return web.Response(text="OK", status=200)
-
-async def handle_crocopay(request: web.Request):
-    bot: Bot = request.app['bot']
-    system = "crocopay"
-    try:
-        data = await request.json()
-        logger.info(f"Получен вебхук: {data}")
-        try:
-            QR = data['qr_sbp_url']
-            if QR:
-                return (f"Вам выпал QR!", f"{QR}")
-        except:
-            QR = None
-        order_id = request.match_info['order_id']
-        amount = data['total']
-        logger.info(f"{order_id}")
-        chat_id = await get_chat_id(order_id, system)
-        logger.info(f"{chat_id}")
-        try:
-            try:
-                await bot.send_message(
-                    chat_id=chat_id,
-                    text=f"🟢CrocoPay:\n✅Заказ №{order_id} на сумму {amount}₽ успешно оплачен!"
-                )
-                await add_paid_order(float(amount), chat_id, "crocopay")
-            except:
-                logger.info(f"Ошибка1: {e}")    
-        except Exception as e:   
-                logger.info(f"Ошибка2: {e}")
-
-        return web.Response(text="OK", status=200)
-    
-    except Exception as e:
-        logger.error(f"Ошибка3: {str(e)}")
-        return web.Response(text="OK", status=200)
-
 async def handle_all_other(request):
     return web.Response(text="Forbidden", status=403)
 
 async def get_chat_id(order_id, system):
-    if system == "corkpay":
-        async with connect("/root/paybots/corkpay/orders_corkpay.db") as db:
-            cursor = await db.execute(
-                "SELECT chat_id, amount FROM orders_corkpay WHERE order_id = ?", 
-                (order_id,)
-            )
-            result = await cursor.fetchone()
-    elif system == "epay":
-        async with connect("/root/paybots/epay/orders_epay.db") as db:
-            cursor = await db.execute(
-                "SELECT chat_id, amount FROM orders_epay WHERE order_id = ?", 
-                (order_id,)
-            )
-            result = await cursor.fetchone()
-    elif system == "crocopay":
-        async with connect("/root/paybots/crocopay/orders_crocopay.db") as db:
-            cursor = await db.execute(
-                "SELECT chat_id FROM orders_crocopay WHERE order_id = ?", 
-                (order_id,)
-            )
-            result = await cursor.fetchone()
-            result = result[0]
-    logger.info(f"{result}")
-    return result
+    db_path = DB_PATHS.get(system)
+    if not db_path:
+        logger.error(f"Неизвестная система: {system}")
+        return None, None
+
+    async with aiosqlite.connect(db_path) as db:
+        query = "SELECT chat_id, amount FROM orders_{system} WHERE order_id = ?"
+        cursor = await db.execute(query, (order_id,))
+        result = await cursor.fetchone()
+
+    if result:
+        return (result[0], result[1]) if len(result) > 1 else (result[0], None)
+    return None, None
 
 async def auto_cleanup():
     while True:
         try:
-            systems = {
-                "corkpay": "/root/paybots/corkpay/orders_corkpay.db",
-                "epay": "/root/paybots/epay/orders_epay.db",
-                "crocopay": "/root/paybots/crocopay/orders_crocopay.db",
-            }
-            
-            for system, db_path in systems.items():
-                cutoff = (datetime.now() - timedelta(days=10)).isoformat()
+            cutoff = (datetime.now() - timedelta(days=10)).isoformat()
+            for system, db_path in DB_PATHS.items():
                 async with aiosqlite.connect(db_path) as db:
                     await db.execute(
                         f"DELETE FROM orders_{system} WHERE timestamp < ?",
                         (cutoff,))
                     await db.commit()
-            
+
             logger.info("Автоочистка старых заказов выполнена")
         except Exception as e:
             logger.error(f"Ошибка автоочистки: {e}")
@@ -452,26 +375,25 @@ async def add_paid_order(amount: float, chat_id: int, system: str):
         await db.commit()
 
 async def main():
-    logger = logging.getLogger(__name__)
-    asyncio.create_task(auto_cleanup())
-    asyncio.create_task(schedule_report())
+    logger.info("Запуск задач автоочистки и отчетов")
+    asyncio.gather(auto_cleanup(), schedule_report())
     await create_paid_orders_table()
     try:
         logger.info("Удаление старого вебхука...")
         await bot.delete_webhook()
         logger.info(f"Настройка вебхука: https://{DOMAIN}/tg_webhook")
-        await bot.set_webhook(
-            url=f"https://{DOMAIN}/tg_webhook"
-        )
+        await bot.set_webhook(url=f"https://{DOMAIN}/tg_webhook")
+
         web_app = await start_web_app(dp, bot)
         setup_application(web_app, dp, bot=bot)
         runner = web.AppRunner(web_app)
         await runner.setup()
-        site = web.TCPSite(runner, '0.0.0.0', 8080) 
+        site = web.TCPSite(runner, '0.0.0.0', 8080)
         await site.start()
         logger.info("Сервер запущен на порту 8080")
+
         while True:
-            await asyncio.sleep(3600)  
+            await asyncio.sleep(3600)
     except Exception as e:
         logger.error(f"Ошибка: {e}", exc_info=True)
     finally:
