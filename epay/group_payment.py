61f1099614f1ad63590205c868422e4cab3309f1
AzikMrazik <AzikMrazik@yandex.ru>
2025-06-26 22:10:15 +0500

    Update group_payment.py

--- epay/group_payment.py
+++ epay/group_payment.py
@@ -54,24 +54,25 @@ async def gen_command(message: Message):
         link = p[2]
         amt = p[1]
         
-        qr = qrcode.QRCode(version=1, box_size=20, border=8)
+        qr = qrcode.QRCode(version=1, box_size=25, border=6)
         qr.add_data(link)
         qr.make(fit=True)
         
         img = qr.make_image(fill_color="black", back_color="white")
+        img = img.convert("RGBA")
         
         try:
-            resp = requests.get("https://i.otzovik.com/objects/b/2340000/2335986.png", timeout=5)
-            logo = Image.open(io.BytesIO(resp.content))
+            r = requests.get("https://i.otzovik.com/objects/b/2340000/2335986.png", timeout=5)
+            logo = Image.open(io.BytesIO(r.content))
             logo = logo.convert("RGBA")
-            logo = logo.resize((80, 80))
+            logo = logo.resize((90, 90))
         except:
-            logo = Image.new('RGBA', (80, 80), (255, 255, 255, 255))
+            logo = Image.new('RGBA', (90, 90), (255, 255, 255, 255))
             d = ImageDraw.Draw(logo)
-            d.rectangle([10, 10, 70, 70], fill=(0, 100, 255, 255))
+            d.rectangle([5, 5, 85, 85], fill=(255, 0, 0, 255))
         
         pos = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
-        img.paste(logo, pos, logo if logo.mode == 'RGBA' else None)
+        img.paste(logo, pos, logo)
         
         bio = io.BytesIO()
         img.save(bio, format='PNG')
